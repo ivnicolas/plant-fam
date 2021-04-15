4f577a54034785433b07274db0df510a17284990
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-04-15 16:04:57 -0400

    finished basic routes

--- app/controllers/plant_lists_controller.rb
+++ app/controllers/plant_lists_controller.rb
@@ -1,12 +1,12 @@
 class PlantListsController < ApplicationController
     get '/:listname' do 
-        binding.pry
+        # binding.pry
         @listname=params[:listname]
         # @plant_lists = session[:user_plant_list]
         # @list_items= @plant_lists.collect{|list| list.plant_id} #this gives an array of of the plant ids
         # @list_items.collect {|plant| Plant.all.find_by_id(plant)}
         @user=User.find_by_id(session[:user_id])
-        @plant_directory=Plants.all
+        @plant_directory=Plant.all
         # binding.pry
         erb :"plant_list/plantlists"
     end 
